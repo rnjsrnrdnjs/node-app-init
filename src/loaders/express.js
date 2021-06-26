228cf9629a5e203453d1264728e76079a4cf0440
090.js <49832278+rnjsrnrdnjs@users.noreply.github.com>
2021-06-26 10:03:14 +0900

    Update express.js

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -45,8 +45,8 @@ module.exports = (app) => {
         app.use(morgan('dev'));
     }
 
-    app.use(express.static(path.join(__dirname, 'public')));
-    app.use('/img', express.static(path.join(__dirname, 'uploads')));
+    app.use(express.static(path.join(__dirname, '../public')));
+    app.use('/img', express.static(path.join(__dirname, '../uploads')));
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
     app.use(cookieParser(process.env.COOKIE_SECRET));
