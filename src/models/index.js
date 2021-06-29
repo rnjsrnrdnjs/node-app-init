d3f0ec78925b1f62d816100b46a099b69a0a2e5a
090.js <49832278+rnjsrnrdnjs@users.noreply.github.com>
2021-06-29 19:52:02 +0900

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env];
+const User = require('./user');
 
 const db = {};
 const sequelize = new Sequelize(
